eeafc64ca90cf4d44fc4cd7a0b818c36bb5cfca6
martingerardoc <martingerardoc56@gmail.com>
2026-05-20 00:05:59 -0300

    dynamic product page

--- src/js/product.js
+++ src/js/product.js
@@ -2,6 +2,31 @@ import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
+const product = await dataSource.findProductById(productId);
+
+const params = new URLSearchParams(window.location.search);
+
+const productId = params.get("product");
+
+
+function productTemplate(product) {
+  return `
+    <h3>${product.Name}</h3>
+
+    <img
+      src="${product.Image}"
+      alt="${product.Name}"
+    />
+
+    <p>${product.Description}</p>
+
+    <p>$${product.FinalPrice}</p>
+
+    <button id="addToCart" data-id="${product.Id}">
+      Add to Cart
+    </button>
+  `;
+}
 
 function addProductToCart(product) {
   let cartItems = localStorage.getItem("so-cart");
@@ -26,3 +51,6 @@ async function addToCartHandler(e) {
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
+
+  document.querySelector(".product-detail").innerHTML =
+  productTemplate(product);
